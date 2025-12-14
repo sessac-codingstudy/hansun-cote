6be450b9175599f22183fee633f210e42a0eccfa
한선우 <gkstjsdn0011@naver.com>
2025-12-15 01:56:43 +0900

    cote

--- main.py
+++ main.py
@@ -2,28 +2,58 @@ import sys
 
 input = sys.stdin.readline
 
-amount = int(input())
+target = int(input())
+n = int(input())
+temp_arr = list(map(int, input().split()))
+n_dict = {}
+m_dict = {}
 
-r_price = 1000 - amount 
+arr_n = []
+for t in temp_arr:
+    if t not in n_dict:
+        n_dict[t] = 1
+    else:
+        n_dict[t] += 1
 
-m_500 = r_price //500 
-r_price = r_price % 500
+arr_n.append(temp_arr[0])
+for i in range(1, len(temp_arr)):
+    arr_n.append(arr_n[i - 1] + temp_arr[i])
 
-m_100 = r_price //100
-r_price = r_price % 100 
+m = int(input())
+temp_arr = list(map(int, input().split()))
 
-m_50 = r_price // 50 
-r_price = r_price % 50 
+for t in temp_arr:
+    if t not in m_dict:
+        m_dict[t] = 1
+    else:
+        m_dict[t] += 1
 
-m_10 = r_price // 10 
-r_price = r_price % 10 
+arr_m = []
+arr_m.append(temp_arr[0])
+for i in range(1, len(temp_arr)):
+    arr_m.append(arr_m[i - 1] + temp_arr[i])
 
-m_5 = r_price // 5 
-r_price = r_price % 5
+for i in range(len(arr_n) - 1):
+    for j in range(i + 1, len(arr_n)):
+        sub_sum = arr_n[j] - (arr_n[i - 1] if i > 0 else 0)
+        if sub_sum in n_dict:
+            n_dict[sub_sum] += 1
+        else:
+            n_dict[sub_sum] = 1
+for i in range(len(arr_m) - 1):
+    for j in range(i + 1, len(arr_m)):
+        sub_sum = arr_m[j] - (arr_m[i - 1] if i > 0 else 0)
+        if sub_sum in m_dict:
+            m_dict[sub_sum] += 1
+        else:
+            m_dict[sub_sum] = 1
 
-m_1 = r_price // 1 
+answer = 0
+# print(n_dict, m_dict)
+for k, v in n_dict.items():
+    if target - k in m_dict:
+        # print("k t-k ", k, target - k)
+        b = m_dict[target - k]
+        answer += v * b
 
-print(m_500 + m_100 + m_50 + m_10 + m_5 + m_1)
-
-
-
+print(answer)
