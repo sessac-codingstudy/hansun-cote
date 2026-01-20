a4bcf9001bbf26b4ea0ee21d3b12672f5e31deaa
han sun woo <gkstjsdn0011@naver.com>
2026-01-20 21:47:56 +0900

    cote

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
-import sys
+def infinite_sequence():
+    num = 0
+    while True:
+        yield num
+        num += 1
 
-input = sys.stdin.readline
-
-count = int(input())
-arr = [] 
-for i in range(count) :
-    num = int(input())
-    if(num == 0) :
-        arr.pop()
-    else :
-        arr.append(num) 
-
-print(sum(arr))
+# 무한 루프에 빠지지 않고, 필요할 때만 번호를 하나씩 따올 수 있음
+gen = infinite_sequence()
+print(next(gen)) # 0
+print(next(gen)) # 1
+# ... 계속해서 호출 가능
